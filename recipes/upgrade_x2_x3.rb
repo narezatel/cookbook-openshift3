d96199430c631cda4012b6c55cf61f3eab878670
Ian Miell <ian.miell@gmail.com>
2017-04-20 18:05:41 +0100

    upgrade - include signer certs in creation, and just redeploy certs on upgrade

---
+++ recipes/upgrade_x2_x3.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: cookbook-openshift3
+# Recipe:: upgrade_x2_x3
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+include_recipe 'cookbook-openshift3::adhoc_redeploy_certs'
